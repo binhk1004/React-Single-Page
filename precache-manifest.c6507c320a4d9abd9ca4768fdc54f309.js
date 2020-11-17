42d14029f30d47466721ca1d41ad7ba64392fc9a
Bin Hyun <binhk1004@naver.com>
2020-11-17 19:49:44 +0900

    Updates

--- precache-manifest.c6507c320a4d9abd9ca4768fdc54f309.js
+++ precache-manifest.c6507c320a4d9abd9ca4768fdc54f309.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9128d8ea30486d5bf2c3104a60cee02f",
+    "revision": "e8469bf6a6daf2aa307ce4797a9fdbfd",
     "url": "/React-Single-Page/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/React-Single-Page/static/css/2.af3c1da9.chunk.css"
   },
   {
-    "revision": "1d9e3207042e96c11435",
+    "revision": "afa415023f7b58ba71c2",
     "url": "/React-Single-Page/static/css/main.be0a0271.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/React-Single-Page/static/js/2.a64c777c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "1d9e3207042e96c11435",
-    "url": "/React-Single-Page/static/js/main.9d5f3eec.chunk.js"
+    "revision": "afa415023f7b58ba71c2",
+    "url": "/React-Single-Page/static/js/main.60899b4f.chunk.js"
   },
   {
     "revision": "a837d4d88a96017e40d8",
